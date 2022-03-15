708500b8e90f454c1fab40b9d057d15b6309b1f2
Harsh Desai <16807421+hurshd0@users.noreply.github.com>
2022-03-15 18:34:48 -0500

    feat: added codepipeline

--- cdk_workshop_app/pipeline_stack.py
+++ cdk_workshop_app/pipeline_stack.py
@@ -1,14 +1,24 @@
 from constructs import Construct
 from aws_cdk import Stack
 from aws_cdk.pipelines import CodePipeline, CodePipelineSource, ShellStep
-
+import aws_cdk as cdk
 
 class WorkshopPipelineStack(Stack):
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        pipeline = CodePipeline(self, "Pipeline", 
-                                pipeline_name = "CDKWorkshopPipeline",
-                                synth=ShellStep(
-                                    "Synth",
-                                                )
+        pipeline = CodePipeline(
+            self,
+            "Pipeline",
+            pipeline_name="CDKWorkshopPipeline",
+            synth=ShellStep(
+                "Synth",
+                input=CodePipelineSource.git_hub("hurshd0/cdk-workshop-app", "main"),
+                authentication=cdk.SecretValue.secrets_manager("GithubToken")
+                commands=[
+                    "npm install -g aws-cdk",
+                    "python -m pip install -r requirements.txt",
+                    "cdk synth",
+                ],
+            ),
+        )
