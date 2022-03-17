b65ed84021dac60bf382600d12639e3a8d0cfabd
Harsh Desai <16807421+hurshd0@users.noreply.github.com>
2022-03-17 14:15:41 -0500

    fix: added missing endpoint properties

--- cdk_workshop_app/pipeline_stage.py
+++ cdk_workshop_app/pipeline_stage.py
@@ -8,3 +8,14 @@ class WorkshopPipelineStage(Stage):
         super().__init__(scope, id, **kwargs)
 
         service = CdkWorkshopAppStack(self, "WebService")
+
+        self._hc_endpoint = service.hc_endpoint
+        self._hc_viewer_url = service.hc_viewer_url
+
+    @property
+    def hc_endpoint(self):
+        return self._hc_endpoint
+
+    @property
+    def hc_viewer_url(self):
+        return self._hc_viewer_url
